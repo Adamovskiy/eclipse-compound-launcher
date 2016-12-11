5a93178d2064127748c8a3764be798362447e168
Alexey Adamovskiy <a.adamovskiy@gmail.com>
2016-12-12 00:55:04 +0300

    Simple launch

--- src/info/adamovskiy/compound/CompoundLaunchConfigurationDelegate.java
+++ src/info/adamovskiy/compound/CompoundLaunchConfigurationDelegate.java
@@ -6,22 +6,30 @@ import java.util.stream.Collectors;
 
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.SubProgressMonitor;
 import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.debug.core.model.LaunchConfigurationDelegate;
+import org.eclipse.debug.internal.ui.DebugUIPlugin;
 
 public class CompoundLaunchConfigurationDelegate extends LaunchConfigurationDelegate {
     @Override
     public void launch(ILaunchConfiguration configuration, String mode, ILaunch launch, IProgressMonitor progressMonitor) throws CoreException {
-        progressMonitor.beginTask("stub", IProgressMonitor.UNKNOWN);
         final List<ILaunchConfiguration> configurations = configuration
                 .getAttribute(ConfigurationKeys.CONFIGS_KEY, new ArrayList<>()).stream()
                 .map(ConfigurationUtils::deserialize).collect(Collectors.toList());
 
-        for (ILaunchConfiguration subConfig : configurations) {
-            System.out.println(subConfig.getType().getName() + "#" + subConfig.getName());
-        }
+        progressMonitor.beginTask("Compound launch", 100 * configurations.size());
+        try {
+            // TODO support async launch
+            for (ILaunchConfiguration subConfig : configurations) {
+                // TODO allow to configure mode override for specific sub-configurations
+                System.out.println(subConfig.getType().getName() + "#" + subConfig.getName());
 
-        progressMonitor.done();
+                DebugUIPlugin.buildAndLaunch(subConfig, mode, new SubProgressMonitor(progressMonitor, 100));
+            }
+        } finally {
+            progressMonitor.done();
+        }
     }
 }
